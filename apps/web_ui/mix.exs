8ba1cf9e5bada526987678e715770696650fb963
Kerry Buckley <kerryjbuckley@gmail.com>
2017-02-14 10:09:35 +0000

    Loosen dependency version requirements

--- apps/web_ui/mix.exs
+++ apps/web_ui/mix.exs
@@ -32,12 +32,12 @@ defmodule WebUi.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.1"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_html, "~> 2.6"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:phoenix_slime, "~> 0.8.0"},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+    [{:phoenix, ">= 1.2.1"},
+     {:phoenix_pubsub, ">= 1.0.0"},
+     {:phoenix_html, ">= 2.6.0"},
+     {:phoenix_live_reload, ">= 1.0.0", only: :dev},
+     {:phoenix_slime, ">= 0.8.0"},
+     {:gettext, ">= 0.11.0"},
+     {:cowboy, ">= 1.0.0"}]
   end
 end
